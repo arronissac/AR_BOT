07fd032c3f17a233a610a367d10a75f9ae93a233
ArronIssac <arronissac@gmail.com>
2018-08-12 14:03:30 +0530

    AI

---
+++ Assets/vbscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class vbscript : MonoBehaviour,IVirtualButtonEventHandler{
+	public GameObject vbbtn;
+	// Use this for initialization
+	void Start () {
+		vbbtn=gameObject.find("playmusic");
+		vbbtn.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+	
+	// Update is called once per frame
+	public void OnbuttonPressed( VirtualButtonBehaviour vb)
+	{
+		Debug.Log ("Buttom pressed");
+	}
